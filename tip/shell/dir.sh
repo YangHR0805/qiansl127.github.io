8d495d4253f44a344b9ba73f6ec59456313f3cf0
Shilei <shilei.qian@gmail.com>
2017-02-07 09:58:17 +0800

    Clean code and change format

--- tip/shell/dir.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DIR=`dirname "$0"`
-DIR=`cd "${DIR}"; pwd`
-
-echo ${DIR}
--- tip/shell/regulaExpression.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-website="http://www.google.com/cn"
-pattern="^http://([^/]+)"
-if [[ $website =~ $pattern ]]; then
-  echo ${BASH_REMATCH[0]}
-  echo ${BASH_REMATCH[1]}
-else
-  echo "Match Failed"
-fi
